5b6b7a3e30c9f64a4d86a48dcccc987c5657e892
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-02-03 13:37:23 -0800

    update navbar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,17 @@
 import photo from "../images/worldwebsite.png";
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	background-color: black;
+	background-image: url(${photo});
+	background-repeat: no-repeat;
+	width: 2000px;
+	height: 2000px;
+`;
 
 function Main() {
-	return (
-		<div className="Main">
-			<header className="main__header">
-				<img className="main__photo" src={photo} />
-			</header>
-		</div>
-	);
+	return <Container></Container>;
 }
 
 export default Main;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,76 @@
 import React from "react";
-const Nav = styled.Navbar`
-	background-color: black;
-	border-bottom: 3px solid hsla(0, 0%, 77%, 0.1);
-	max-height: 80px;
-	padding: 19px 104px 10px;
-`;
-const Navbar = () => (
-	<Nav className="navbar">
-		<button>News App</button>
-		<div>
-			<button style={{ marginRight: "15px" }}>Home</button>
-			<button>Search</button>
-		</div>
-	</Nav>
-);
+import styled from "styled-components";
+import logo from "../images/cutelogo.png";
 
-export default Navbar;
+const HomeButton = styled.button`
+	border-bottom: 3px solid #fff;
+	background-color: initial;
+	color: #fff;
+	font-size: 18px;
+	font-weight: 500;
+	line-height: 24px;
+	margin-left: 700px;
+	padding: 0 9px 23px;
+	text-decoration: none;
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
+const Logo = styled.img`
+	width: 250px;
+	height: 150px;
+	margin-left: 100px;
+`;
+const Nav = styled.div`
+	background-color: black;
+	border-bottom: 3px solid hsla(10, 0%, 90%, 0.3);
+	max-height: 200px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 20px;
+	@media (max-width: 768px) {
+		flex-direction: column;
+		align-items: center;
+	}
+`;
+
+const NavbarLeft = styled.div`
+	display: flex;
+
+	@media (max-width: 768px) {
+		margin-bottom: 10px;
+	}
+`;
+const SignButton = styled.button`
+	background-color: initial;
+	border: 1px solid #fff;
+	border-radius: 100px;
+	color: #fff;
+	font-size: 18px;
+	font-weight: 500;
+	height: 48px;
+	line-height: 24px;
+	margin-top: 45px;
+	margin-right: 300px;
+	padding: 12px;
+	width: 176px;
+	&:hover {
+		cursor: pointer;
+	}
+`;
+function NavBar() {
+	return (
+		<>
+			<Nav>
+				<Logo src={logo} />
+				<NavbarLeft>
+					<HomeButton>Home</HomeButton>
+					<SignButton>Sign in </SignButton>
+				</NavbarLeft>
+			</Nav>
+		</>
+	);
+}
+export default NavBar;
