435fdd059d1b0c65ed1479e243162c908c9f543e
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-02-02 21:20:09 -0800

    i styledcomponent

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React from "react";
-
+const Nav = styled.Navbar`
+	background-color: black;
+	border-bottom: 3px solid hsla(0, 0%, 77%, 0.1);
+	max-height: 80px;
+	padding: 19px 104px 10px;
+`;
 const Navbar = () => (
-	<nav className="navbar">
+	<Nav className="navbar">
 		<button>News App</button>
 		<div>
 			<button style={{ marginRight: "15px" }}>Home</button>
 			<button>Search</button>
 		</div>
-	</nav>
+	</Nav>
 );
 
 export default Navbar;
